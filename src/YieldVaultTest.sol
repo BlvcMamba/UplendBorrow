ba2d458cacffb441fdad612968f277c52b8d78e7
Ishaq Nasir <ishaqnasir47@gmail.com>
2025-09-04 02:14:54 +0100

    YieldVault Contract update

--- src/YieldVaultTest.sol
+++ src/YieldVaultTest.sol
@@ -13,5 +13,34 @@ contract YieldVault {
         protocolFee = _protocolFee;
     }
 
-    
+    //deposit function
+    //@param  the amount to be deposited when trying to interact with this protocol
+    function deposit() external payable {
+        require( msg.value > 0, "Sould deposit amount greater than a zero amount");
+        balancesOfUser[msg.sender] += msg.value;
+        totalAssets += msg.value;
+    }
+
+    // the function that allows for a user to withdraw the amount they have deposited
+    //@params the amount of the deposited funds to be withdrawn
+    function withdrawal(uint256 _amount) external {
+        require(balancesOfUser[msg.sender] >= _amount, "Insufficient balance");
+        //ccalculating the protocol fee
+
+        uint256 fee = (_amount * protocolFee) / FEE_DENOMINATOR;
+        uint256 payout = _amount - fee;
+
+        //Using the check effect interaction pattern
+        balancesOfUser[msg.sender] -= _amount;
+        totalAssets -= _amount;
+
+
+        payable(msg.sender).transfer(payout);
+    }
+
+    //the function the view the amount the user has left in the protocol
+    //@param the address of the user
+    function balanceOf(address _user) external view returns (uint256) {
+        return balancesOfUser(_user);
+    }
 }
